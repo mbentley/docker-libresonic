9f30c9de1887893b7f7b1c53dedf36cf59b4ae4b
Matt Bentley <mbentley@mbentley.net>
2017-12-31 08:21:26 -0500

    Formatting because i am picky
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+echo "======================================"
+echo "Starting libresonic initialization."
+
 if [ ! -d /data ]
 then
   echo "Directory /data doesn't exist; this is not expected!"
   exit 1
+else
+  echo "INFO - found directory /data"
 fi
 
 (cd /data
@@ -13,9 +18,11 @@ for DIR in db lucene2 lastfmcache thumbs music Podcast playlists .cache .java
 do
   if [ ! -d "${DIR}" ]
   then
-    printf "%s not found; creating..." ${DIR}
+    printf "WARN - %s missing; creating..." ${DIR}
     mkdir "${DIR}"
     echo "done"
+  else
+    echo "INFO - found directory ${DIR}"
   fi
 done
 
@@ -23,10 +30,15 @@ for FILE in libresonic.properties libresonic.log rollback.sql
 do
   if [ ! -f "${FILE}" ]
   then
-    printf "%s not found; creating..." ${FILE}
+    printf "WARN - %s missing; creating..." ${FILE}
     touch "${FILE}"
     echo "done"
+  else
+    echo "INFO - found file ${FILE}"
   fi
 done)
+
+echo "libresonic initialization complete!"
+echo "======================================";echo
 
 exec "${@}"
